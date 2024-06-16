cd88b905f830a69c0edbf8841320883e9e9618d0
moistywall <mahemazero@gmail.com>
2024-06-16 22:41:26 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use axum::{
+    body::Bytes,
+    extract::MatchedPath,
+    http::{Request, HeaderMap, StatusCode},
+    response::{Html, Response, IntoResponse},
+    routing::{get, post},
+    Router,
+    Json,
+};
+use std::time::Duration;
+use tokio::net::TcpListener;
+use tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer};
+use tracing::{info, info_span, Span};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                "kakeibo=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // ルーティングを作成,どのパスでどのサービスへたどり着くかを設定する
+    // getで受ける関数は最低限ブラウザで処理可能なテキスト(str?)をを返していればok
+    // ルートがない場合は404を返す
+    let app = Router::new()
+        .route("/", get(root))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    let matched_path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
+
+                    info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        matched_path,
+                        some_other_field = tracing::field::Empty,
+                    )
+                })
+                .on_request(|_request: &Request<_>, _span: &Span| {
+                    info!("request")
+                })
+                .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
+                    info!("response")
+                })
+                .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
+                    // ...
+                })
+                .on_eos(
+                    |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
+                        // ...
+                    },
+                )
+                .on_failure(
+                    |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+                        // ...
+                    },
+                ),
+        );
+
+    // hyperでアプリを起動する
+    let listener = TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> Html<&'static str> {
+    Html("<h1>Web家計簿解析アプリ</h1>")
+}
+
+#[derive(Deserialize)]   
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: i64,
+    username: String,
+}
