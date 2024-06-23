dd5d695e985941a072e766d87a1cdfc7eab8a33c
moistywall <mathemazero@gmail.com>
2024-06-23 23:59:00 +0900

    totyu p.135

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,7 +1,7 @@
-use axum::{extract::Extension, http::StatusCode, response::IntoResponse, Json};
+use axum::{extract::{Extension, Path}, http::StatusCode, response::IntoResponse, Json};
 use std::sync::Arc;
 
-use crate::repositories::{CreateItem, ItemRepository};
+use crate::repositories::{CreateItem, ItemRepository, UpdateItem};
 
 pub async fn create_item<T: ItemRepository>(
     Extension(repository): Extension<Arc<T>>,
@@ -10,3 +10,17 @@ pub async fn create_item<T: ItemRepository>(
     let item = repository.create(payload);
     (StatusCode::CREATED, Json(item))
 }
+
+pub async fn find_item<T: ItemRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    todo!();
+    Ok(StatusCode::OK)  // 暫定OK
+}
+
+pub async fn update_item<T: ItemRepository>(
+    Path(id): Path<i32>,
+    Json(payload): Json<UpdateItem>),
+    Extension(repositoriry): Extension<Arc<T>>,
+) 
